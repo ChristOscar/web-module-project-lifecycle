56072d6f629aa9f790a127690683797666d00c19
Christwide Oscar <triple101.games@gmail.com>
2022-01-11 16:22:01 -0600

    Finished building Wirefram for DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  
+  
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    console.log('App: Render DOM')
+    return (
+      <div>
+        <div className='Header'>
+          <h1>GitHub Card Version 2</h1>
+        </div>
+        <div className='SearchBar'>
+          <form> 
+            <input/>
+            <button>Search</button>
+          </form>
+        </div>
+        <div className='User'>
+          <h1>Searched User</h1>
+        </div>
+        <div className='Follower'>
+          <h2>Searched Users List of Followers</h2>
+        </div>
+      </div>
+      
+    );
   }
 }
 
