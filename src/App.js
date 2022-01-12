c4671be4e234492a8f26f3aed80310be32a46358
Christwide Oscar <triple101.games@gmail.com>
2022-01-11 18:29:08 -0600

    setting up state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import axios from 'axios';
+import User from './components/User'
 
 class App extends React.Component {
   
+  state = {
+    avatar_url: '',
+    name: '',
+    login:'',
+    public_repos: '',
+    followers: '',
+  }
+  componentDidMount(){
+    console.log('App: Component has Mounted');
+    axios.get(`https://api.github.com/users/ChristOscar`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          ...this.state,
+          avatar_url: res.data.avatar_url,
+          name: res.data.name,
+          login: res.data.login,
+          public_repos: res.data.public_repos,
+          followers: res.data.followers
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      
+        ...this.state,
+        login: e.target.value,
+        
+    });
+  }
+  handleSearch = (e) => {
+    console.log('App: Change State.')
+    //1. get our login input.
+    //2. do an axios call
+    //3. save the response from the call to our state
+    const login =  this.state.login;
+    axios.get(`https://api.github.com/users/${login}`)
+    .then(resp =>{
+      this.setState({
+        ...this.state,
+        name: resp.data.name,
+        login:''
+      })
+    })
+  }
   
   render() {
     console.log('App: Render DOM')
@@ -13,16 +63,23 @@ class App extends React.Component {
         </div>
         <div className='SearchBar'>
           <form> 
-            <input/>
-            <button>Search</button>
+            <input value={this.state.login} onChange={this.handleChange}/>
+            <button onClick={this.handleSearch}>Search</button>
           </form>
         </div>
         <div className='User'>
           <h1>Searched User</h1>
+          <h1>{this.state.name}</h1>
+          <img src='https://avatars.githubusercontent.com/u/3699469?v=4'></img>
+          <h3>{this.state.name}</h3>
+          <h4>{this.state.name}</h4>
+          <h5>{this.state.name}</h5>
+          <p></p>
         </div>
         <div className='Follower'>
           <h2>Searched Users List of Followers</h2>
         </div>
+        <User/>
       </div>
       
     );
